26cd4f567a725fa57cb0d10a42dd856765594ffd
grighth12 <grighth12@gmail.com>
2021-10-03 04:20:30 +0900

    update webpack config for SPA

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
-const path = require("path");
-const { VueLoaderPlugin } = require("vue-loader");
-const HtmlPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   resolve: {
-    extensions: [".vue", ".js"],
+    extensions: ['.vue', '.js'],
     alias: {
-      "~": path.resolve(__dirname, "src"),
+      '~': path.resolve(__dirname, 'src'),
     },
   },
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', // index.html 경로 절대경로
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: "vue-loader",
+        use: 'vue-loader',
       },
       {
         test: /\.s?css$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"],
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
     }),
     new CopyPlugin({
       patterns: [
-        { from: "static" }, // to는 생략해도 output 경로로 들어감
+        { from: 'static' }, // to는 생략해도 output 경로로 들어감
       ],
     }),
   ],
+  devServer:{
+    historyApiFallback: true, // SPA 새로고침시 404 문제 해결
+  }
 };
